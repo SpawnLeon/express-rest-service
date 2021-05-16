a1139059f677db88722ec756b0550e5057c9186b
Alexander Leonov <spawnleon@gmail.com>
2021-05-16 22:58:13 +0300

    fix: refactor

--- src/resources/users/user.service.js
+++ src/resources/users/user.service.js
@@ -1,17 +1,17 @@
 const usersRepo = require('./user.memory.repository');
-const taskService = require("../tasks/task.service");
+const taskService = require('../tasks/task.service');
 
 const getAll = () => usersRepo.getAll();
 
 const postUser = (userData) => usersRepo.postUser(userData);
 
-const getUser = (id) => usersRepo.getUser(id)
+const getUser = (id) => usersRepo.getUser(id);
 
-const updateUser = (id, userData) => usersRepo.updateUser(id, userData)
+const updateUser = (id, userData) => usersRepo.updateUser(id, userData);
 
 const removeUser = async (id) => {
   await taskService.removeTasksFromUser(id);
   return usersRepo.removeUser(id);
-}
+};
 
 module.exports = { getAll, postUser, getUser, updateUser, removeUser };
